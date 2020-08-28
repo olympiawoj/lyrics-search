6f25bee32e5b73165ee4cd18a70eb18fb9291b9a
olympiawoj <owojcik7@gmail.com>
2020-08-28 09:37:08 -0400

    Update DOM & Add Pagination

--- script.js
+++ script.js
@@ -7,19 +7,73 @@ const apiURL = "https://api.lyrics.ovh";
 
 // Search by song or artist
 async function searchSongs(term) {
-//   fetch(`${apiURL}/suggest/${term}`)
-//     .then((res) => res.json())
-//     .then((data) => console.log(data));
+  //   fetch(`${apiURL}/suggest/${term}`)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
 
-    const res = await fetch(`${apiURL}/suggest/${term}`)
-    const data = await res.json()
-    // console.log(data)
-    showData(data)
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+  // console.log(data)
+  showData(data);
 }
 
 // Show song and artist
-function showData(data){
-    
+function showData(data) {
+  //   let output = "";
+  //   data.data.forEach((song) => {
+  //     output += `
+  //         <li>
+  //             <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //             <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //         </li>
+  //         `;
+  //   });
+  //   result.innerHTML = `
+  //         <ul class="songs">
+  //             ${output}
+  //         </ul>
+  //     `;
+
+  result.innerHTML = `
+    <ul class="songs">
+        ${data.data
+          .map(
+            (song) => `
+        <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>
+        `
+          )
+          .join("")}
+    </ul>
+    `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+            ${
+              data.prev
+                ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+                : ""
+            }
+            ${
+              data.next
+                ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+                : ""
+            }
+        `;
+  } else {
+    more.innerHTML = "";
+  }
+
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
 }
 
 // Event listeners
