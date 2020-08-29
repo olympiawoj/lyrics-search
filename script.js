b48a59376f6160cd83e3a18006edd81e867bba9b
olympiawoj <owojcik7@gmail.com>
2020-08-29 17:01:44 -0400

    Get & Display Song Lyrics

--- script.js
+++ script.js
@@ -76,6 +76,18 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+// Get lyrics
+async function getLyrics(artist, songTitle){
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>'); // replace takes in a REGEX 
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`
+
+  more.innerHTML = '';
+}
+
 // Event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -86,3 +98,14 @@ form.addEventListener("submit", (e) => {
     searchSongs(searchTerm);
   }
 });
+
+
+// Get lyrics button click
+result.addEventListener('click', (e)=>{
+  const clickedEl = e.target
+  if(clickedEl.tagName === 'BUTTON'){
+    const artist = clickedEl.getAttribute('data-artist')
+    const songTitle = clickedEl.getAttribute('data-songtitle')
+    getLyrics(artist, songTitle)
+  }
+})
